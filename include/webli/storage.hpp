695653afeeb5b0f0c999701a3eb007be095a0e68
MINA <mina@minaqwq.dev>
2024-08-07 00:53:43 +0200

    Storage: loadAsString

--- include/webli/storage.hpp
+++ include/webli/storage.hpp
@@ -2,9 +2,13 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace W::Storage {
 std::string loadAsString(std::string_view path) noexcept;
-}
+
+std::vector<std::uint8_t> loadAsBinary(std::string_view path) noexcept;
+} // namespace W::Storage
--- src/storage.cpp
+++ src/storage.cpp
@@ -23,4 +23,22 @@ std::string loadAsString(std::string_view path) noexcept {
 
   return buffer;
 }
+
+std::vector<std::uint8_t> loadAsBinary(std::string_view path) noexcept {
+  std::vector<std::uint8_t> buffer{};
+  std::size_t size;
+  std::fstream stream{path.data(), std::ios::in};
+
+  if (!stream.is_open()) {
+    return buffer;
+  }
+
+  std::filesystem::path fspath(path);
+
+  size = std::filesystem::file_size(fspath);
+  buffer.resize(size);
+  stream.read(reinterpret_cast<char *>(buffer.data()), size);
+
+  return buffer;
+}
 } // namespace W::Storage
