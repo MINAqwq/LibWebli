07f2c2177eadc6b6dce959c1306a7c5a592075e4
MINA <mina@minaqwq.dev>
2024-08-06 14:52:15 +0200

    Added WebExceptions throwing http responses

--- include/webli/exceptions.hpp
+++ include/webli/exceptions.hpp
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <webli/http.hpp>
+
 namespace W {
 class Exception {
 public:
@@ -11,4 +13,23 @@ public:
 private:
   const char *msg;
 };
+
+namespace WebException {
+class HttpException {
+public:
+  HttpException(const Http::Response &resp) : resp(resp) {}
+
+  Http::Response &getResponse() { return resp; }
+
+private:
+  Http::Response resp;
+};
+
+class NotFound : public HttpException {
+public:
+  NotFound()
+      : HttpException(Http::Response(Http::StatusCode::NotFound, {},
+                                     "<h1>Not Found</h1>")) {}
+};
+} // namespace Exceptions
 } // namespace W
